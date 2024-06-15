8549db6c34d2623da1836d40185a3e1d42b2fa68
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-06-15 11:03:27 -0400

    CV component built

---
+++ src/cv.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import '../src/styles/cv.css'
+
+function cv() {
+    return (
+        <>
+            
+        </>
+    )
+}
--- src/education.jsx
+++ src/education.jsx
@@ -1,11 +1,58 @@
 import { useState } from 'react'
 import '../src/styles/education.css'
 
+function FormItem1({ label, placeholder, optional }) {
+    const props = {
+        label:  label,
+        placeholder: placeholder,
+        optional: optional, 
+    }
+
+    return (
+        <div className='row'>
+            <label htmlFor={label}>
+                {label}
+                {optional && <small>optional</small>}
+                </label>
+            <input 
+                type="text" 
+                id={label}
+                placeholder={placeholder}
+            />
+        </div>
+    )
+}
+
+FormItem1.defaultProps
+
+function FormItem2({ label, placeholder1, placeholder2 }) {
+    return (
+        <>
+        
+        </>
+    )
+}
+
 function educationInformation() {
+    /*
+    const formInputs = { 
+        // Label: string : 
+        // [ multi-input: boolean, 
+        // optional-tag: boolean, 
+        // placeholder: string, 
+        // secondary-placeholder: string **Optional ]
+        "School" : [false, false, "Enter School/University"],
+        "Degree" : [false, false, "Enter Degree/Field of Study"],
+        "Start/End Date:" : [true, false, "Start Date", "End Date"],
+        "Extra Information:" : [false, true, "Enter any addional information"],
+    }*/
+
     return (
         <div className="educationForm">
             <h2>Education</h2>
             <form action="GET">
+
+
                 <div className="row">
                     <label htmlFor="school">
                         School:
@@ -16,6 +63,8 @@ function educationInformation() {
                         placeholder='Enter School/University'
                      />
                 </div>
+
+
                 <div className="row">
                     <label htmlFor="degree">
                         Degree:
