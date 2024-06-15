51fd80f2941f680dc417e661d7b3451b05336f68
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-06-15 14:50:33 -0400

    Add props to cv sections for reusability

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,38 +1,94 @@
 import { useState } from 'react'
+import education from '../index' // Update eventually 
 import EmailLogo from '../assets/email.svg'
 import PhoneLogo from '../assets/phone.svg'
 import LocationLogo from '../assets/location.svg'
 import '../styles/cv.css'
 
+const personalInfo = {
+    name: "John Doe",
+    email: "John.Doe@email.com",
+    phone: "+1(614)-555-5555",
+    location: "Columbus, OH",
+}
+
+const professionalExperience = {
+    sectionTitle: "Professional Experience",
+    date: "07/2022 - 12/2023",
+    location: "Cleveland, OH",
+    title: "Cleveland Clinic",
+    info: "Clinical Microbiology Laboratory",
+}
+
 function CV() {
     return (
         <div className="cv">
-            <CVHeading />
+            <CVHeading 
+                name={personalInfo.name}
+                email={personalInfo.email}
+                phone={personalInfo.phone}
+                location={personalInfo.location}    
+            />
+            <CVSection 
+                title={education.sectionTitle}
+                contents={education}
+            />
+            <CVSection 
+                title={professionalExperience.sectionTitle}
+                contents={professionalExperience}
+            />
         </div>
     )
 }
 
-function CVHeading() {
+function CVHeading({ name, email, phone, location }) { // Add props for name, email, phone, location
     return (
-        <>
         <div className="heading">
-            <h1 className="name">John Doe</h1>
+            <h1 className="name">{name}</h1>
             <div className="personal-information">
                 <div className="email">
                     <img src={EmailLogo} alt="Email Logo" />
-                    John.Doe@email.com
+                    {email}
                 </div>
                 <div className="phone">
                     <img src={PhoneLogo} alt="Phone Logo" />
-                    (614)-555-5555
+                    {phone}
                 </div>
                 <div className="location">
                     <img src={LocationLogo} alt="Location Logo" />
-                    Columbus, OH
+                    {location}
                 </div>
             </div>
         </div>
-        </>
+    )
+}
+
+function CVSection({ title, contents }) { 
+    return (
+        <div className="section">
+            <h2>{title}</h2>
+            <InformationPiece 
+                date={contents.date}
+                location={contents.location}
+                title={contents.title}
+                info={contents.info}
+            />
+        </div>
+    )
+}
+
+function InformationPiece({ date, location, title, info }) {
+    return (
+        <div className="piece">
+            <div className="date-location">
+                <p className="date">{date}</p>
+                <p className="location">{location}</p>
+            </div>
+            <div className="title-info">
+                <h3 className="title">{title}</h3>
+                <p className="info">{info}</p>
+            </div>
+        </div>
     )
 }
 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const education = {
+    sectionTitle: "Education",
+    date: "08/2018 - 05/2022",
+    location: "Columbus, OH",
+    title: "The Ohio State University",
+    info: "Bachelor's of Science",
+}
+
+export default education
