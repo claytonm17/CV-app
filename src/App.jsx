b34c01864b375b205f77223f51a2a380471da2fd
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-06-16 17:25:46 -0400

    Education form layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <>
             <CV data={formData} />
             <PersonalInfoForm onFormDataChange={handleFormChange} />
+            <EducationForm onFormDataChange={handleFormChange} />
         </>
     )
 }
--- src/components/EducationDetails.jsx
+++ src/components/EducationDetails.jsx
@@ -2,10 +2,42 @@ import { useState } from 'react'
 import '../styles/form.css'
 
 function Form() {
+
+    const [formState, setFormState] = useState({ univ: "", startDate: "", endDate: "", location: "", info: "" });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        const newFormState = { ...formState, [name]: value };
+        setFormState(newFormState);
+        onFormDataChange(newFormState);
+    };
+
+    const oneTextInput = (label, name, placeholder) => (
+        <div className="single-row">
+            <label className="input-row" htmlFor="email">
+            {label}
+                <input 
+                    type="text"
+                    id = 'input'
+                    name = {name}
+                    placeholder={placeholder}
+                    minLength={5}
+                    maxLength={100}
+                    value={formState.name}
+                    onChange={handleChange}
+                />
+            </label>
+        </div>
+    )
+
     return (
-        <>
-        
-        </>
+        <div className="educationInformationForm">
+            <h2>Education Information</h2>
+            <form>
+                {oneTextInput("College/University:", "univ", "The Ohio State University")}
+                {oneTextInput("College/University:", "univ", "The Ohio State University")}
+            </form>
+        </div>
     )
 }
 
